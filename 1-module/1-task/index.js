22d6023d1250289623e8de50930eea4bc9e66bae
TroAA <A.Troshnev@bssys.com>
2022-12-15 20:13:16 +0300

    Решил второй блок задач

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 1 || n === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 0; i < n - 1; i++) {
+      result *= n - i;
+    }
+    return result;
+  }
 }
