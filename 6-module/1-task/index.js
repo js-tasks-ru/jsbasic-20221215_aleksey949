7f9dcd8cc92f078acdb2239e51ffb8f7ae4de109
TroAA <A.Troshnev@bssys.com>
2023-01-19 16:06:35 +0300

    Решил шестой блок задач В index.html ругался анализатор кода, что не указан язык + поправил "кракозябрики" на форме

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,26 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `<thead><tr><td>Имя</td><td>Возраст</td><td>Зарплата</td><td>Город</td><td></td></tr></thead>`;
+
+    let tableInner = this.rows.map((row) =>
+      // Необходимо перебрать каждый ряд и оттуда извлечь все значения
+      `<tr>${Object.values(row).map((value) =>
+        `<td>${value}</td>`
+      ).join('')}<td><button>X</button></td></tr>`
+    ).join('');
+    this.elem.innerHTML += `<tbody>
+${tableInner}
+</tbody>`;
+    this.elem.addEventListener('click', (event) => this.removeTd(event));
+  }
+
+  removeTd(event) {
+    if (event.target.tagName === 'BUTTON') {
+      let tr = event.target.closest('tr');
+      tr.remove();
+    }
   }
 }
